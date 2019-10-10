73b6c02b064bee19a25a492585e70ba87991eaa3
witty-banner-1950 <asimsali95@gmail.com>
2019-10-10 16:34:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,7 +3,7 @@ class School
 
   def initialize(school_name)   #initialize method includes school_name and roster equal to empty hash
     @school_name = school_name #part 1
-    @roster = {}    #empty hash
+    @roster = {}    #empty hash {key => value}
   end
 
   def roster    #getter method for roster/ part 2
@@ -19,13 +19,13 @@ class School
     @roster[grade] << name    #pushes name to empty array based on which grade given as argument
   end
 
-  def grade(grade)     #method to output names based on grade
+  def grade(grade)     #method to output names based on grade given
     @roster[grade]
   end
 
-  def sort
+  def sort            #method to sort each grade's names in alpahabetical order
     @roster.each do |grade, name|
-      @roster[grade] = name.sort
+      @roster[grade] = name.sort    #.sort reposnsible for alpahabetical
     end
   end
 end
